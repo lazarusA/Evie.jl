7f5bc139eff4e32e83e8631a4d7d9a89a790bfc2
Lazaro Alonso <lazarus.alon@gmail.com>
2025-03-31 21:28:32 +0200

    update versions

--- docs/example.jl
+++ docs/example.jl
@@ -19,10 +19,11 @@ listenToMe(1.2, buf_obs, txt_obs, circ_buf, model_att;
     transcribe_text=true) # Q. What is love?
 
 # connect to Llama2
+using Llama2
 llama_model = joinpath(@__DIR__, "models/llama-2-7b-chat.Q4_K_S.gguf")
 model_llama = load_gguf_model(llama_model);
 
-sample(model_llama, "What is love?"; temperature=0.7f0) # txt_obs[]
+sample(model_llama, "how to sum two numbers in Julia?"; temperature=0.7f0) # txt_obs[]
 
 output_prompt=[]
 sampleObs(model_llama, "What is love?", output_prompt; temperature=0.7f0) # txt_obs[]
--- src/Evie.jl
+++ src/Evie.jl
@@ -7,7 +7,6 @@ using Whisper # https://github.com/lazarusA/Whisper.jl.git
 using Llama2: load_gguf_model, sample, sampleObs # https://github.com/lazarusA/Llama2.jl.git
 export load_gguf_model, sample, sampleObs
 
-# Write your package code here.
 include("listen.jl")
 include("plot_spectrogram.jl")
 include("transcribe_stream.jl")
