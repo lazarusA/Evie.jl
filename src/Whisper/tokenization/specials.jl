c1bf8e0a2740f5cab189dcd557f27ca9856ca94d
Lazaro Alonso <lazarus.alon@gmail.com>
2026-06-28 19:21:37 +0200

    load audio but decoding is not right

--- src/Whisper/tokenization/specials.jl
+++ src/Whisper/tokenization/specials.jl
@@ -1,12 +1,18 @@
+using Printf
+
 const LANGUAGES = OrderedDict(
     "en" => "english",
     "de" => "german",
     "fr" => "french",
     "es" => "spanish"
+    # add more as needed
 )
 
 const SPECIALS = vcat(
     ["<|endoftext|>", "<|startoftranscript|>"],
-    ["<|$(v)|>" for v in values(LANGUAGES)],
-    ["<|translate|>", "<|transcribe|>"]
+    ["<|$lang|>" for lang in keys(LANGUAGES)],      # keys = language codes
+    ["<|translate|>", "<|transcribe|>"],
+    ["<|startoflm|>", "<|startofprev|>"],
+    ["<|nospeech|>", "<|notimestamps|>"],
+    ["<|$(@sprintf("%.2f", i * 0.02))|>" for i in 0:1500],  # timestamp tokens
 )
