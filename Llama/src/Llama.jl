c977a48d8763eaa915fae7cdf94584759c6cfad5
Lazaro Alonso <lazarus.alon@gmail.com>
2026-06-30 14:01:15 +0200

    llama template

---
+++ Llama/src/Llama.jl
@@ -0,0 +1,11 @@
+module Llama
+
+greet() = print("Hello World!")
+
+export load_gguf_model, sample, sampleObs
+
+function load_gguf_model end
+function sample end
+function sampleObs end
+
+end # module Llama
